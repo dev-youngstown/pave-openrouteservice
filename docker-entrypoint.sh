b167870eb6aa29b0fa548bb587e940296b037b73
nicolasjott <nicobox2002@gmail.com>
2024-01-29 15:14:18 -0500

    update resource

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -58,15 +58,15 @@ if [ ! -f "${ors_base}/ors-core/data/osm_file.pbf" ]; then
 fi
 
 Using environment variables for database details
-DB_NAME=${NF_PAVE_GISDB_DATABASE}
-DB_USER=${NF_PAVE_GISDB_USERNAME}
-DB_PASS=${NF_PAVE_GISDB_PASSWORD}
-DB_HOST=${NF_PAVE_GISDB_HOST}
-DB_PORT=${NF_PAVE_GISDB_PORT}
+DB_NAME=${NF_ORS_GISDB_DATABASE}
+DB_USER=${NF_ORS_GISDB_USERNAME}
+DB_PASS=${NF_ORS_GISDB_PASSWORD}
+DB_HOST=${NF_ORS_GISDB_HOST}
+DB_PORT=${NF_ORS_GISDB_PORT}
 
 # Import OSM data into PostgreSQL using environment variables
 echo "Importing OSM data into ${DB_NAME}"
-echo $DB_PASS | osm2pgsql -c -d $DB_NAME -U $DB_USER -W -H $DB_HOST -P $DB_PORT -C 8192 -G --hstore ${ors_base}/ors-core/data/osm_file.pbf
+echo $DB_PASS | osm2pgsql -c -d $DB_NAME -U $DB_USER -W -H $DB_HOST -P $DB_PORT -C 16384 -G --hstore ${ors_base}/ors-core/data/osm_file.pbf
 
 # so docker can stop the process gracefully
 exec "${catalina_base}"/bin/catalina.sh run
